920ba08ed560d593e7c66ec944249fdc4fc94ef8
hanssav <[handiirawanub@gmail.com]>
2022-02-05 00:40:28 +0700

    cloudinary

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const express = require("express");
 
 const cors = require('cors')
 
+// import this
+const http = require('http');
+
 const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_URL, // we must define cors because our client and server have diffe
+  },
+});
 
 const router = require("./src/routes/routes");
 
@@ -13,8 +23,16 @@ const port = process.env.PORT || 5001;
 app.use(express.json());
 app.use(cors())
 
+
 app.use("/api/v1/", router);
 
 app.use("/uploads", express.static("uploads"))
+
+app.get('/', function (req, res) {
+    res.send({
+      message: 'Hello World',
+      CLIENT_URL: process.env.CLIENT_URL,
+    });
+  });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
